84000953e1fb7126409ecb86d889006da6d019d9
pro <tatpongp.itesm@gmail.com>
2024-08-23 12:43:23 +0700

    init commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,7 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base:  '/mini_poon/'    },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - mini_poon',
